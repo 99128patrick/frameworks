e79ba8c45c9e149dbef3d4cc1ca0c28d38b29d33
99128patrick <127446550+99128patrick@users.noreply.github.com>
2023-03-21 16:16:34 -0400

    Add files via upload

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+nome = 'patrick'
+count = 0;
+
+sub (){
+  this.count--;
+
+}
+
+soma(){
+  this.count++;
+}
+
+
+
 }
